ccc6ddcb3998a1e2d7dacdc7a8a4efed6d0dbbe8
dynomite567 <dynomitecentral@outlook.com>
2017-05-05 06:53:55 -0700

    Added back the .help command
    Also Bug fixes

--- app.js
+++ app.js
@@ -22,7 +22,6 @@ client.on('ready', () => {
 
 client.on('message', message => {
   var args = message.content.split(/[ ]+/);
-  console.log(hasRole(message.member, "BotMod"));
   if (commandIs ("clear", message)){
     if (hasRole(message.member, "BotMod") || hasRole(message.member, "BotAdmin")) {
       if (args.length === 101) {
@@ -46,6 +45,11 @@ client.on('message', message => {
     message.channel.sendMessage('Join the official Uniqulem server here for updates and support: https://discord.gg/m4q24gX');
   }
 
+  if (commandIs ("help", message)) {
+    message.channel.sendMessage('Commands:');
+    message.channel.sendMessage('.help .hello .server .info .clear')
+  }
+
   if (commandIs ("hello", message)) {
     message.channel.sendMessage('Hello! I am a bot! *Beep Boop!* To find out more about me use .info!');
   }
@@ -53,7 +57,7 @@ client.on('message', message => {
   if (commandIs ("info", message)) {
     message.channel.sendMessage('I was made by @DynomiteCentral#4808 by hand to serve this and many more servers!');
     message.channel.sendMessage('If you want a changelog, help devolop the bot, or even add the bot to your server, come to the official github site here: https://dynomite567.github.io/Uniqulem/');
-    message.channel.sendMessage('```Current Version: v1.0.2```');
+    message.channel.sendMessage('```Current Version: v1.1.1```');
   }
 
 });
