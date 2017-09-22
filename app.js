97449e844abf84bfec3c8105c3578ebcc1a65fde
dynomite567 <dynomitecentral@outlook.com>
2017-09-22 07:50:32 -0700

    Fixed the clear command
    You now must enter the number 2 to delete 1 message for now.

--- app.js
+++ app.js
@@ -1,5 +1,12 @@
 const Discord = require("discord.js");
+const fs = require("fs");
+const superagent = require("superagent");
+const nodeschedule = require("node-schedule");
 const client = new Discord.Client();
+
+var accounts = JSON.parse(fs.readFileSync("./accounts.json", "utf8"));
+var daily = JSON.parse(fs.readFileSync("./daily.json", "utf8"));
+var owner = "190210389496037376";
 
 function commandIs(str, msg){
   return msg.content.toLowerCase().startsWith(";" + str);
@@ -23,14 +30,30 @@ function hasRole(mem, role) {
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.username}!`);
   client.user.setGame(`;help | On ${client.guilds.size} guilds!`);
+  superagent.post('https://bots.discordlist.net/api')
+  .type('form')
+  .send({'token': "token"})
+  .send({'servers': client.guilds.size})
+  .end(function(err, res) {
+  if (err) {
+    console.log(err)
+  } else {
+    console.log('Updated Guild Count on discordlist.net!')
+  }
+})
 });
 
 client.on('message', message => {
   var args = message.content.split(/[ ]+/);
+  var account = accounts.find(function(item) {
+    return item.user === message.author.id;
+});
+
   if (commandIs ("clear", message)){
+    message.delete(1);
     if (hasRole(message.member, "BotMod") || hasRole(message.member, "BotAdmin")) {
       if (args.length >= 3) {
-          message.channel.sendMessage('Number of messages are not defined or the number is two large! Usage: ;clear (Number of messages to delete)');
+          message.channel.send('Number of messages are not defined or the number is two large! Usage: ;clear (Number of messages to delete)');
       } else {
         var msg;
         if (args.length === 1){
@@ -38,33 +61,36 @@ client.on('message', message => {
         } else {
           msg=parseInt(args[1]);
         }
-        message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(sum(1,messages))).catch(console.error);
+        message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
       }
     } else {
-      message.channel.sendMessage("You are not a **BotMod** or **BotAdmin**");
+      message.channel.send("You are not a **BotMod** or **BotAdmin**");
     }
   }
 
   if (commandIs ("say", message)){
+    message.delete(1);
     if (hasRole(message.member, "BotMod") || hasRole(message.member, "BotAdmin")) {
       if(args.length === 1){
-        message.channel.sendMessage('Please give me something to say! Usage: ;say (Message)');
+        message.channel.send('Please give me something to say! Usage: ;say (Message)');
       } else {
-        message.channel.sendMessage(args.join(" ").substring(5));
+        message.channel.send(args.join(" ").substring(5));
       }
     } else {
-      message.channel.sendMessage("You are not a **BotMod** or **BotAdmin**");
+      message.channel.send("You are not a **BotMod** or **BotAdmin**");
     }
   }
 
 
   if (commandIs ("server", message)) {
-    message.channel.sendMessage('Join the official Uniqulem server here for updates and support: https://discord.gg/m4q24gX');
+    message.delete(1);
+    message.channel.send('Join the official Uniqulem server here for updates and support: https://discord.gg/m4q24gX');
   }
 
   if (commandIs ("help", message)) {
-    message.channel.sendMessage('Commands:');
-    message.channel.sendMessage('``;help ;hello ;server ;info ;clear``')
+    message.delete(1);
+    message.channel.send('Commands:');
+    message.channel.send('``;help ;hello ;server ;info ;clear``')
   }
 
   if (commandIs ("hello", message)) {
@@ -74,7 +100,7 @@ client.on('message', message => {
   if (commandIs ("info", message)) {
     message.channel.sendMessage('I was made by @DynomiteCentral#4808 by hand to serve this and many more servers!');
     message.channel.sendMessage('If you want a changelog, help devolop the bot, or even add the bot to your server, come to the official github site here: https://dynomite567.github.io/Uniqulem/');
-    message.channel.sendMessage('```Current Version: v1.1.2```');
+    message.channel.sendMessage('```Current Version: v1.1.4b```');
   }
 
 });
