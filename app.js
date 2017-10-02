860df7ef5191bb27a47e2ddf25e5455441076ec6
dynomite567 <dynomitecentral@outlook.com>
2017-10-02 13:03:01 -0700

    Added Embeds

--- app.js
+++ app.js
@@ -2,14 +2,16 @@ const Discord = require("discord.js");
 const fs = require("fs");
 const superagent = require("superagent");
 const nodeschedule = require("node-schedule");
+const config = require("./config.json");
 const client = new Discord.Client();
+
 
 var accounts = JSON.parse(fs.readFileSync("./accounts.json", "utf8"));
 var daily = JSON.parse(fs.readFileSync("./daily.json", "utf8"));
 var owner = "190210389496037376";
 
 function commandIs(str, msg){
-  return msg.content.toLowerCase().startsWith(";" + str);
+  return msg.content.toLowerCase().startsWith("{" + str);
 }
 
 function sum(x,y){
@@ -32,7 +34,7 @@ client.on('ready', () => {
   client.user.setGame(`;help | On ${client.guilds.size} guilds!`);
   superagent.post('https://bots.discordlist.net/api')
   .type('form')
-  .send({'token': "token"})
+  .send({'token': token})
   .send({'servers': client.guilds.size})
   .end(function(err, res) {
   if (err) {
@@ -53,7 +55,7 @@ client.on('message', message => {
     message.delete(1);
     if (hasRole(message.member, "BotMod") || hasRole(message.member, "BotAdmin")) {
       if (args.length >= 3) {
-          message.channel.send('Number of messages are not defined or the number is two large! Usage: ;clear (Number of messages to delete)');
+          message.channel.send(new Discord.RichEmbed().setTitle("Error").setAuthor(message.author.username, message.author.avatarURL).setColor("#ff252a").setDescription("Number of messages are not defined or the number is too large! Usage: ``;clear (Number of messages to delete)``"));
       } else {
         var msg;
         if (args.length === 1){
@@ -64,7 +66,7 @@ client.on('message', message => {
         message.channel.fetchMessages({limit: msg}).then(messages => message.channel.bulkDelete(messages)).catch(console.error);
       }
     } else {
-      message.channel.send("You are not a **BotMod** or **BotAdmin**");
+      message.channel.send(new Discord.RichEmbed().setTitle("Permision Denied").setAuthor(message.author.username, message.author.avatarURL).setColor("#ff252a").setDescription("You are not a **BotMod** or **BotAdmin**"));
     }
   }
 
@@ -72,37 +74,29 @@ client.on('message', message => {
     message.delete(1);
     if (hasRole(message.member, "BotMod") || hasRole(message.member, "BotAdmin")) {
       if(args.length === 1){
-        message.channel.send('Please give me something to say! Usage: ;say (Message)');
+        message.channel.send(new Discord.RichEmbed().setTitle("Error").setAuthor(message.author.username, message.author.avatarURL).setColor("#ff252a").setDescription("I need something to say! Usage: ``;say (message)``"));
       } else {
         message.channel.send(args.join(" ").substring(5));
       }
     } else {
-      message.channel.send("You are not a **BotMod** or **BotAdmin**");
+      message.channel.send(new Discord.RichEmbed().setTitle("Permision Denied").setAuthor(message.author.username, message.author.avatarURL).setColor("#ff252a").setDescription("You are not a **BotMod** or **BotAdmin**"));
     }
   }
 
-
   if (commandIs ("server", message)) {
     message.delete(1);
-    message.channel.send('Join the official Uniqulem server here for updates and support: https://discord.gg/m4q24gX');
-  }
-
+    message.channel.send(new Discord.RichEmbed().setTitle("Support Server").setAuthor(message.author.username, message.author.avatarURL).setColor("#BB00FF").setDescription("Join the official Uniqulem server here for updates and support: https://discord.gg/m4q24gX"));
+}
   if (commandIs ("help", message)) {
     message.delete(1);
-    message.channel.send('Commands:');
-    message.channel.send('``;help ;hello ;server ;info ;clear``')
-  }
-
-  if (commandIs ("hello", message)) {
-    message.channel.sendMessage('Hello! I am a bot! *Beep Boop!* To find out more about me use .info!');
+    message.channel.send(new Discord.RichEmbed().setTitle("Help").setAuthor(message.author.username, message.author.avatarURL).setColor("#BB00FF").setDescription("Here are the commands. More info about these commands can be found by clicking help above.").setFooter("Created by DynomiteCentral", "http://i.imgur.com/Qo6BP2v.png").setThumbnail("http://i.imgur.com/Qo6BP2v.png").setTimestamp().setURL("https://dynomite567.github.io/Uniqulem/").addField("Admin / Mod","``;say (message)`` ``;clear (number)``").addField("Info","``;info`` ``;hello`` ``;server`` ``;help``"));
   }
 
   if (commandIs ("info", message)) {
-    message.channel.sendMessage('I was made by @DynomiteCentral#4808 by hand to serve this and many more servers!');
-    message.channel.sendMessage('If you want a changelog, help devolop the bot, or even add the bot to your server, come to the official github site here: https://dynomite567.github.io/Uniqulem/');
-    message.channel.sendMessage('```Current Version: v1.1.4b```');
+    message.delete(1);
+    message.channel.send(new Discord.RichEmbed().setTitle("Info").setAuthor(message.author.username, message.author.avatarURL).setColor("#BB00FF").setDescription("I was made by @DynomiteCentral#4808 by hand to serve this and many more servers!\nIf you want a changelog, help devolop the bot, or even add the bot to your server, come to the official github site here: https://dynomite567.github.io/Uniqulem/\n```Current Version: v1.1.4b```").setThumbnail("http://i.imgur.com/Qo6BP2v.png"));
   }
 
 });
 
-client.login('token');
+client.login("token");
